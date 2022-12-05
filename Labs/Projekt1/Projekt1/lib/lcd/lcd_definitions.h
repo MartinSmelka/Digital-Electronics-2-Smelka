4be0f4435d29f0e4d7ec25f6aea9a1a617b171c6
MartinSmelka <mato.smelka@gmail.com>
2022-12-05 18:57:39 +0100

    Projekt Work

--- Labs/Projekt1/Projekt1/lib/lcd/lcd_definitions.h
+++ Labs/Projekt1/Projekt1/lib/lcd/lcd_definitions.h
@@ -68,9 +68,9 @@
 #define LCD_DATA2_PIN   PD6  /**< @brief Pin for HD44780 data pin D6 */
 #define LCD_DATA3_PIN   PD7  /**< @brief Pin for HD44780 data pin D7 */
 #define LCD_RS_PORT     PORTB
-#define LCD_RS_PIN      PB4
+#define LCD_RS_PIN      PB0
 #define LCD_E_PORT      PORTB
-#define LCD_E_PIN       PB5
+#define LCD_E_PIN       PB1
 // R/W pin is connected to GND on LCD Keypad Shield
 
 
