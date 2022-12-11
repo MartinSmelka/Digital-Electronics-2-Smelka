e33d9d537768919c5eab780657e0b2fb888c94b7
MartinSmelka <mato.smelka@gmail.com>
2022-12-11 20:51:58 +0100

    Projekt2 Works 1

---
+++ Labs/Project2_Lock/README.md
@@ -0,0 +1,132 @@
+# Project 2
+
+Application of 2 Servos, rotary encoder, and Digilent PmodCLP LCD module.
+
+## Instructions
+
+The goal of the project is cooperation in pairs, further study of the topic, design of own solutions, implementation, creation of project documentation and presentation of results. The division of roles and tasks within the team is up to their members.
+
+* Students work on a project in the labs during the 10th and 11th weeks of the semester.
+
+* Through the BUT e-learning, students submit a link to the GitHub repository, which contains the complete project in PlatfomIO and a descriptive README file. The submission deadline is the day before the next laboratory, i.e. in 2 weeks.
+
+* The source code for the AVR must be written in C and/or Assembly and must be implementable on Arduino Uno board using toolchains from the semester. No other development tools are allowed.
+
+## Recommended GitHub repository structure
+
+   ```c
+   YOUR_PROJECT        // PlatfomIO project
+   ├── include         // Included files
+   ├── lib             // Libraries
+   ├── src             // Source file(s)
+   │   └── main.c
+   ├── test            // No need this
+   ├── platformio.ini  // Project Configuration File
+   └── README.md       // Report of this project
+   ```
+
+## Recommended README.md file structure
+
+### Team members
+
+* Martin Šmelka(App Design, Code, Device Planning)
+* Šimon Špánik(Device Planning, Documentation)
+
+## Hardware description
+
+With the help of specified hardware components and Arduino, we created an implementation where we use the Encoder and Joystick to control the LEDs in the shape of a cross and display the data on the LCD.
+
+### Schematic
+
+![image]()
+
+Components are connected to Arduino Uno like on the schematic. 
+It's used HD44780 LCD display due to missing Digilent PmodCLP LCD module in SimullDE softwere.
+
+LEDs are connected in active low mode. Joystick is in position of up-right corner to demonstrate the function of circuit.
+
+### Encoder
+
+#### Description
+
+KY-040 is a rotary encoder that provides an output of how much it was turned and in which direction. The supply voltage is 5V and has 20 pulses per 360 degrees. It also includes a switch with a digital output.
+
+Pinout:
+
+![image](https://user-images.githubusercontent.com/99726477/205434930-50e16daf-f3ed-4ef1-aaff-bf3b9e7298a7.png)
+
+Image of two data signals which determine a direction of rotating (taken from datasheet):
+
+![image](https://user-images.githubusercontent.com/99726477/205435020-c9411c68-0b3f-4a49-8d00-867d629956a3.png)
+
+
+#### Function
+
+Enconder in our project controls the blinking speed of the LEDs. It is driven in both directions. When pressed, the output is reset and the flashing is turned off.
+
+
+### Digilent PmodCLP LCD module
+
+#### Description
+
+16x2 character LCD display with 8-bit parallel data interface. Supply voltage is 5V. Its controlled by 10-bit instructions. From LSB to MSB are 8-bits of data and the next 2-bits are Read and Write operating bits.
+
+![image](https://user-images.githubusercontent.com/99726477/205435246-52842a16-c7c0-4014-881b-cd2b4b707686.png)
+
+Pinout:
+
+![image](https://user-images.githubusercontent.com/99726477/205435281-7e80ee4f-f1b4-459c-ac3e-b6893d463a5d.png)
+
+
+#### Function
+
+The LCD displays the coordinates of XY axis, encoder blink rate value and joystick direction with its custom directional arrow characters
+
+### LEDs
+
+LEDs used in the circuit has RED color, Package: DIL
+
+![image](https://user-images.githubusercontent.com/99726477/205602378-859bb8f7-a3a6-496c-943f-c0729d48a317.png)
+
+### Servo
+
+#### Description
+
+
+
+Pinout:
+
+![image]()
+
+Image of control PWM (taken from datasheet):
+
+![image]()
+
+
+#### Function
+
+
+
+## Software description
+ 1. **Timer:** [timer.h]()
+ 2. **Gpio:** [gpio.c](), [gpio.h])
+ 3. **LCD:** [lcd.c](), [lcd.h](), [lcd_definitions.h]()
+ 4. **Source:** [main.c]()
+
+Put flowchats of your algorithm(s):
+
+![main() flowchart]()
+
+![TIMER (1) drawio]()
+
+![last_flow drawio (1)]()
+
+## Video
+
+[Youtube Link to View the Implementation of the Project]()
+
+## References
+
+1. https://www.tme.eu/Document/07d3a201631d75ac52980710936cebec/410-142P.pdf
+2. INSERT SERVO DATASHEET
+3. https://www.rcscomponents.kiev.ua/datasheets/ky-040-datasheet.pdf
---
+++ Labs/Project2_Lock/src/main.c
@@ -0,0 +1,304 @@
+/***********************************************************************
+ * DE2-Project 1:
+ * Martin Šmelka, Šimon Špánik
+ * Use of 
+ * 
+ * ATmega328P (Arduino Uno), 16 MHz, PlatformIO, KY-040 Rotary encoder, 2x Servo, 4 ressistors for LEDs, 4 LEDs arranged
+ * in Red-Green pairs to denote the status of selected "Lock" servo.
+ *
+ * Copyright (c) 2018 Tomas Fryza
+ * Dept. of Radio Electronics, Brno University of Technology, Czechia
+ * This work is licensed under the terms of the MIT license.
+ * 
+ **********************************************************************/
+
+
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>         // AVR device-specific IO definitions
+#include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
+#include <gpio.h>           // GPIO library for AVR-GCC
+#include "timer.h"          // Timer library for AVR-GCC
+#include <lcd.h>            // Peter Fleury's LCD library
+#include <stdlib.h>         // C library. Needed for number conversions
+#include <twi.h>            // I2C/TWI library for AVR-GCC.
+#include <uart.h>           // Interrupt UART library with receive/transmit circular buffers
+
+// Define LEDS for "Lock" Status 
+#define LED_A_Green PC5
+#define LED_A_Red PC4
+#define LED_B_Green PC3
+#define LED_B_Red PC2
+
+// Define ENCODER for choosing the "Lock" State.
+#define OutputCLK PD1
+#define OutputDT PD2
+#define OutputSW PD3
+
+// Define Servo for PWM output of drive signal of Arduino
+
+
+// Static VARIABLES for functions 
+static int8_t counter = 0; // Counter to Cap the Encoder
+static uint8_t State; // Encoder State 
+static uint8_t Last_State; // Encoder Last State(Used as referral)
+static uint8_t SW_ENC = 1; // Base Digital signal of the encoder Switch
+
+/* Function definitions ----------------------------------------------*/
+/**********************************************************************
+ * Function: Main function where the program execution begins
+ * Purpose:  Ininitialize display, set up pullup and nopull for ENCODER
+ *           Prepare Custom Characters for use on the LCD
+ *           Engage Timer at 4ms(functional for our purposes)
+ *           Enable Global interrupts by setting the global interrupt mask
+ **********************************************************************/
+int main(void)
+{
+    GPIO_mode_input_nopull(&DDRD,OutputCLK);
+    GPIO_mode_input_nopull(&DDRD,OutputDT);
+    GPIO_mode_input_pullup(&DDRD,OutputSW);
+
+    Last_State = GPIO_read(&PIND,OutputCLK);
+
+    // Initialize display
+    lcd_init(LCD_DISP_ON);
+
+    GPIO_mode_output(&DDRC,LED_A_Green);
+    GPIO_mode_output(&DDRC,LED_A_Red);
+    GPIO_mode_output(&DDRC,LED_B_Green);
+    GPIO_mode_output(&DDRC,LED_B_Red);
+
+
+
+
+
+
+ ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Custom character definition using https://omerk.github.io/lcdchargen/ , for Lock Logo.
+// These characters were not found in the dedicated ASCII symbol library.
+// We use 8 bits to store the 1 characters(8 bits per character) in adresse 0x00
+    uint8_t customChar[8] = {
+
+    0b11111,
+    0b10001,
+    0b10001,
+    0b11111,
+    0b10001,
+    0b11011,
+    0b11011,
+    0b11111
+    };
+
+
+    lcd_command(1<<LCD_CGRAM);       // Set addressing to CGRAM (Character Generator RAM)
+                                     // ie to individual lines of character patterns
+    for (uint8_t i = 0; i < 8; i++)  // Copy new character patterns line by line to CGRAM
+        lcd_data(customChar[i]);
+    lcd_command(1<<LCD_DDRAM);       // Set addressing back to DDRAM (Display Data RAM)
+                                     // ie to character codes
+
+
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Configure Analog-to-Digital Convertion unit
+    // Select ADC voltage reference to "AVCC with external capacitor at AREF pin"
+    ADMUX = ADMUX | (1<<REFS0);
+    // Enable ADC module
+    ADCSRA |= (1<<ADEN);
+    // Enable conversion complete interrupt
+    ADCSRA |= (1<<ADIE);
+    // Set clock prescaler to 128
+    ADCSRA |= (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
+   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    // Engage Timer at 4ms(functional for our purposes)
+    TIM1_overflow_4ms();
+    TIM1_overflow_interrupt_enable();
+    
+
+   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // sei Enables interrupts by setting the global interrupt mask
+    sei();
+
+    // Infinite loop to keep operations running
+    while (1)
+    {
+            /* Empty loop. All subsequent operations are performed exclusively 
+         * inside interrupt service routines, ISRs */
+    }
+
+    return 0;
+}
+
+   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/* Function definitions ----------------------------------------------*/
+/**********************************************************************
+ * Function: TIMER1_OVF_vect function where the timer operates and Encoder is set up
+ * Purpose:  Start ADC conversion
+ *           LCD Display set up
+ *           Encoder set up
+ **********************************************************************/
+
+ISR(TIMER1_OVF_vect)
+{
+  char string[4];
+  State = GPIO_read(&PIND,OutputCLK);
+    // Start ADC conversion
+    ADCSRA |= (1<<ADSC);
+
+    // LCD Display
+  lcd_gotoxy(0, 0);
+  lcd_puts("Lock App");
+  lcd_gotoxy(8, 0);
+  lcd_putc(0x00);
+  lcd_gotoxy(0, 1);
+  lcd_puts("State:");
+
+
+    // Encoder checks state to dermine operation to be preformed upon turning the encoder(right from last state +1, left from last state -1)
+  if (State != Last_State && State == 1)
+  {
+
+      if (GPIO_read(&PIND,OutputDT) != State) 
+      {
+      counter ++;
+      }
+      
+      else {
+      counter --;
+      }
+     // Encoder capped at 6 for six states
+      if(counter>6)
+        counter = 6;
+      else if(counter<-6)
+        counter = -6;
+
+      itoa(counter, string, 10);
+      lcd_gotoxy(11, 0);
+      lcd_puts("    "); // Input empty space to refresh LCD cursor for new imput, this is used most cases 
+      
+      lcd_gotoxy(11, 0);
+      lcd_puts(string);
+  }
+
+Last_State = State;
+SW_ENC = GPIO_read(&PIND,OutputSW);
+}
+
+
+/* Function definitions ----------------------------------------------*/
+/**********************************************************************
+ * Function: Locl_vect function where the ADC converts Analog Joystick input into useable Digital 
+ * Purpose:  Adress shift due to single ADC limitation
+ *           Display X and Y variable as they are converted and read
+ *           Set up overflow
+ **********************************************************************/
+
+ISR(ADC_vect)
+{
+  
+  /* Turning all LEDs OFF with permanent High on driving pins ------------------------------------------------*/
+  GPIO_write_high(&PORTC,LED_A_Green);
+  GPIO_write_high(&PORTC,LED_A_Red);
+  GPIO_write_high(&PORTC,LED_B_Green);
+  GPIO_write_high(&PORTC,LED_B_Red);
+
+  
+  /* Blinking LEDS and directional fucntions -----------------------------------------*/
+  /* Using arrays of possible location of the joystic in two dimensions represented by two variables X and Y we can activate coresponding LEDs -----------------------------------------*/
+  if( counter == 0 || counter == 0 )
+  {  
+      lcd_gotoxy(6, 1);
+      lcd_puts("                   "); 
+      lcd_gotoxy(6, 1);
+      lcd_puts("Null");  
+    } 
+  
+  
+  if( counter == 1 || counter == -1 )
+  {  
+      lcd_gotoxy(6, 1);
+      lcd_puts("                   "); 
+      lcd_gotoxy(6, 1);
+      lcd_puts("Unlock A");  
+    }
+    if( counter == 2 || counter == -2 )
+  {  
+      lcd_gotoxy(6, 1);
+      lcd_puts("                 "); 
+      lcd_gotoxy(6, 1);
+      lcd_puts("Lock A");  
+    }
+    if( counter == 3 || counter == -3 )
+  {  
+      lcd_gotoxy(6, 1);
+      lcd_puts("                "); 
+      lcd_gotoxy(6, 1);
+      lcd_puts("Unlock B");  
+    }
+    if( counter == 4 || counter == -4 )
+  {  
+      lcd_gotoxy(6, 1);
+      lcd_puts("               "); 
+      lcd_gotoxy(6, 1);
+      lcd_puts("Lock B");  
+    }
+      if( counter == 5 || counter == -5 )
+  {  
+      lcd_gotoxy(6, 1);
+      lcd_puts("                "); 
+      lcd_gotoxy(6, 1);
+      lcd_puts("Unlock AB");  
+    }
+        if( counter == 6 || counter == -6 )
+  {  
+      lcd_gotoxy(6, 1);
+      lcd_puts("                "); 
+      lcd_gotoxy(6, 1);
+      lcd_puts("Lock AB");  
+    }
+
+if( SW_ENC == 1)
+  {  
+
+    } 
+
+
+if( SW_ENC == 0 && (counter == 1 || counter == -1) )
+  {  
+      GPIO_write_low(&PORTC,LED_A_Green);
+    } 
+
+if( SW_ENC == 0 && (counter == 2 || counter == -2) )
+  {  
+      GPIO_write_low(&PORTC,LED_A_Red);
+    } 
+
+if( SW_ENC == 0 && (counter == 3 || counter == -3) )
+  {  
+      GPIO_write_low(&PORTC,LED_B_Green);
+    } 
+
+if( SW_ENC == 0 && (counter == 4 || counter == -4) )
+  {  
+      GPIO_write_low(&PORTC,LED_B_Red);
+    } 
+
+if( SW_ENC == 0 && (counter == 5 || counter == -5) )
+  {  
+      GPIO_write_low(&PORTC,LED_A_Green);
+      GPIO_write_low(&PORTC,LED_B_Green);
+    } 
+
+if( SW_ENC == 0 && (counter == 6 || counter == -6) )
+  {  
+      GPIO_write_low(&PORTC,LED_A_Red);
+      GPIO_write_low(&PORTC,LED_B_Red);
+    } 
+
+
+
+
+}
